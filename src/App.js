344ba54ca7504dcecbde6db2e33d7ed6d3d49a4a
Eric Austin <ewaustin@gmail.com>
2019-11-22 15:46:52 -0600

    birdfeeder playlist loading and autoplaying

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <iframe width="1920" height="1080" src="https://www.youtube.com/embed/videoseries?list=PLEpflZFGXmE2de3HhZSNmpDK95CS5tV75&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" ></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
